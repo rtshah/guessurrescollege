c9fc6c29042d1ca8682307dde72ccddc18033cda
rtshah <rahultshah@gmail.com>
2024-03-29 01:46:27 -0500

    trying to fix vercel deployment

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 const nextPageRoute = require('./nextPageRoute'); 
 const nextPageRoute2 = require('./nextPageRoute2'); 
 const nextPageRoute3 = require('./nextPageRoute3'); 
